868cc81bff61710052aac1d90d427ed2979f9c62
Cris-div <christian.unocc@tecsup.edu.pe>
2026-05-04 21:32:15 -0500

    despligue

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql'
-    }
-);
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+    dialect: 'mysql'
+});
 
 module.exports = sequelize;
